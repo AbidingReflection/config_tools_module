2b821c3df3d2217f917a7e3f051d85a01a6a7b87
Jake <abidingreflection@gmail.com>
2024-10-04 11:50:45 -0600

    Remove Validation rules to move within project

--- config_tools/validation_rules_auth.py
+++
@@ -1,8 +0,0 @@
-from .config_validator import ConfigValidator
-
-auth_rules  = {
-    'bearer_token': {
-        'required': True,
-        'validator': ConfigValidator.validate_qTest_bearer_token
-    }
-}
